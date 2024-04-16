459c52e09992d8ba7efd7b0861ce8839d6121b5c
Cristian Yair Gutierrez <fistergutierrez@gmail.com>
2024-04-16 10:04:08 -0600

    👔 agregando logica de adivinar palabra - 1ra version

--- AdivinaPalabra/Program.cs
+++ AdivinaPalabra/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string palabraSecreta = "programacion";
+int intentosMaximos = 5;
+int intentosRealizados = 0;
+bool adivinado = false;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("¡Bienvenido al juego de adivinar la palabra!");
+Console.WriteLine("Tienes que adivinar una palabra.");
+
+while (!adivinado && intentosRealizados < intentosMaximos)
+{
+    Console.Write("Introduce tu intento: ");
+    string intento = Console.ReadLine();
+
+    if (intento?.ToLower() == palabraSecreta)
+    {
+        adivinado = true;
+        Console.WriteLine("¡Felicidades! ¡Has adivinado la palabra!");
+    }
+    else
+    {
+        intentosRealizados++;
+        Console.WriteLine("Intento incorrecto. Te quedan " + (intentosMaximos - intentosRealizados) + " intentos.");
+    }
+}
+
+if (!adivinado)
+{
+    Console.WriteLine("¡Has agotado todos tus intentos! La palabra secreta era: " + palabraSecreta);
+}
